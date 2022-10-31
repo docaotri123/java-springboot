3ec30ab1f4823940f08b891f303ce7bd7422d084
TriDC <tridc@innotech-vn.com>
2022-10-31 17:37:27 +0700

    added flyway

--- src/main/java/com/example/demo/controllers/ProductController.java
+++ src/main/java/com/example/demo/controllers/ProductController.java
@@ -1,37 +1,28 @@
 package com.example.demo.controllers;
 
-import com.example.demo.models.Product;
 import com.example.demo.models.ResponseObject;
-import com.example.demo.repositories.ProductRepository;
+import com.example.demo.services.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "v1/products")
 public class ProductController {
     @Autowired
-    private ProductRepository repository;
+    private IProductService productService;
     @GetMapping("")
-    List<Product> products() {
-        return repository.findAll();
+    ResponseEntity<ResponseObject> products() {
+        return productService.products();
     }
 
     @GetMapping("/{id}")
     ResponseEntity<ResponseObject> getById(@PathVariable Long id) {
-        Optional<Product> product = repository.findById(id);
+        return productService.getProductDetail(id);
+    }
 
-        if (product.isPresent()) {
-            return ResponseEntity.status(HttpStatus.OK).body(new ResponseObject( "successfully", product));
-        }
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseObject("error", null));
+    @DeleteMapping("/{id}")
+    ResponseEntity<ResponseObject> deleteById(@PathVariable Long id) {
+        return productService.delete(id);
     }
 }
